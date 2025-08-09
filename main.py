4956accce26cfe7305d1b299c6e78c8b163b3036
Runner89 <alain.login@hotmail.com>
2025-08-09 18:41:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -376,8 +376,13 @@ def webhook():
         balance_response = get_futures_balance(api_key, secret_key)
         logs.append(f"Balance Response: {balance_response}")
         if balance_response.get("code") == 0:
-            balance_data = balance_response.get("data", {}).get("balance", {}) * leverage2
-            available_usdt = float(balance_data.get("availableMargin", 0)) * leverage2
+
+            balance_data_temp = 0
+            #balance_data = balance_response.get("data", {}).get("balance", {})
+            balance_data_temp = float(balance_response.get("data", {}).get("balance", {}).get("availableMargin", 0))
+            balance_data = available_margin * leverage2
+
+            available_usdt = float(balance_data.get("availableMargin", 0)) * leverage 2
             logs.append(f"Freies USDT Guthaben: {available_usdt}")
         else:
             logs.append("Fehler beim Abrufen der Balance.")
