461d6ee2c2e77d447ea504aa181f2d602676edf3
Runner89 <alain.login@hotmail.com>
2025-08-09 18:33:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -427,10 +427,10 @@ def webhook():
                     logs.append(f"Ordergröße aus Cache für {botname} gelöscht (keine offene Sell-Limit-Order)")
 
                 if available_usdt is not None and pyramiding > 0:
-                    usdt_amount = max(((available_usdt - sicherheit) / pyramiding) * leverage2, 0)
-                    saved_usdt_amounts[botname] = usdt_amount
-                    logs.append(f"Neue Ordergröße berechnet: {usdt_amount})
-                    logs.append(firebase_speichere_ordergroesse(botname, usdt_amount, firebase_secret))
+                    usdt_amount = max(((available_usdt - sicherheit) / pyramiding), 0)
+                    saved_usdt_amounts[botname] = usdt_amount * leverage2
+                    logs.append(f"Neue Ordergröße berechnet: {usdt_amount * leverage2})
+                    logs.append(firebase_speichere_ordergroesse(botname, usdt_amount * leverage2, firebase_secret))
 
             saved_usdt_amount = saved_usdt_amounts.get(botname, 0)
 
