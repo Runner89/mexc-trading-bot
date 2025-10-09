7bf989f861afe480d90f4272ffe0e82965b54319
Runner89 <alain.login@hotmail.com>
2025-10-09 19:33:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1651,10 +1651,10 @@ def webhook():
 
 
         
-if __name__ == __main__:
-    # Achtung debug=True in Produktion ausschalten
-    app.run(debug=True, host=0.0.0.0, port=5000)
 
+if __name__ == "__main__":
+    # Achtung: debug=True in Produktion ausschalten
+    app.run(debug=True, host="0.0.0.0", port=5000)
         
         
 
